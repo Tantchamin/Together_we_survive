3f6b141e419697bbf5d3a2717cf6217715c2524f
GFStealer-666 <naroj.joi@mail.kmutt.ac.th>
2023-05-04 02:21:03 +0700

    Updated : SetActiveInfoPanelForMapPlace , Modified : UnityMapScene

---
+++ Assets/Scripts/MapInfoPanelSetActiveScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapInfoPanelSetActiveScript : MonoBehaviour
+{
+    [SerializeField] private GameObject _infoPanel;
+    public void SetActivePanel(){
+        if(_infoPanel == null) return;
+        bool _isPanelActive = _infoPanel.activeSelf;
+        _infoPanel.SetActive(!_isPanelActive);
+    }
+}
