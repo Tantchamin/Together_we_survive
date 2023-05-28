984a1fa9fdbb1c889b7e34f942e4eb47e7dae14f
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-05-28 19:27:46 +0700

    NEW: EatingManageUI in living room NEW: LivingRoomFrontendScript

---
+++ Assets/Scripts/LivingRoomFrontendScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingRoomFrontendScript : MonoBehaviour
+{
+    [SerializeField] private Camera _roomCamera;
+    [SerializeField] private GameObject _livingRoomBG;
+    [SerializeField] private GameObject _livingRoomFrontUI;
+    [SerializeField] private GameObject _eatingManageUI;
+    [SerializeField] private GameObject _endDayButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _livingRoomFrontUI = GameObject.Find("LivingRoomFrontUI");
+        _eatingManageUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_roomCamera.transform.position.x == _livingRoomBG.transform.position.x)
+        {
+            SetLivingRoomUIActive(true);
+        }
+        else
+        {
+            SetLivingRoomUIActive(false);
+        }
+    }
+
+    public void SetLivingRoomUIActive(bool _uiActive)
+    {
+        _livingRoomFrontUI.SetActive(_uiActive);
+    }
+
+    public void DisplayEatingManageUI(bool _isDisplay)
+    {
+        _eatingManageUI.SetActive(_isDisplay);
+        _endDayButton.SetActive(!_isDisplay);
+    }
+}
