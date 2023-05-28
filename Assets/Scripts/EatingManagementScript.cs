6609a6de0374526f5b984d99b5ece6b21a66aa35
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2023-05-28 22:41:53 +0700

    UPDATE: EatingButton interacable depend on resource player have

--- Assets/Scripts/EatingManagementScript.cs
+++ Assets/Scripts/EatingManagementScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EatingManagementScript : MonoBehaviour
 {
     [SerializeField] private CharacterStatScript CharacterStatScript;
     [SerializeField] private KitchenResourceBackendScript kitchenResourceBackendScript;
+    [SerializeField] private Button _rawFoodButton, _canFoodButton, _cookedFoodButton, _waterDrinkButton;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,28 @@ public class EatingManagementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (kitchenResourceBackendScript.GetRawFoodAmount() == 0)
+            _rawFoodButton.interactable = false;
+        else
+            _rawFoodButton.interactable = true;
+
+        if (kitchenResourceBackendScript.GetCannedFoodAmount() == 0)
+            _canFoodButton.interactable = false;
+        else
+            _canFoodButton.interactable = true;
+
+        if (kitchenResourceBackendScript.GetWaterAmount() == 0)
+            _waterDrinkButton.interactable = false;
+        else
+            _waterDrinkButton.interactable = true;
+
+        _cookedFoodButton.interactable = false;
+
     }
 
     public void RawFoodEatButton()
     {
-        kitchenResourceBackendScript.UseRawFood(-1);
+        kitchenResourceBackendScript.UseRawFood(1);
         CharacterStatScript.CharacterHungryAdjust(+2);
         int randomNumber = Random.Range(1, 101);
         if(randomNumber  <= 50)
